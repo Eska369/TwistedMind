ecacc58fd4f54d8846096d1cd2b52e2353042c3f
Marcin <marcin8eska@gmail.com>
2021-01-09 17:01:05 +0100

    Dialog, chodzenie, skakanie, przesuwanie kodem działa B)

---
+++ Assets/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class Cube : MonoBehaviour
+{
+    public NPCConversation myConversation;
+
+    public GameObject siekiera;
+    
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ConversationManager.Instance.StartConversation(myConversation);
+            Debug.Log("dziala to czy nie wrr");
+            siekiera.transform.Rotate(0.0f, 0.0f, 45.0f);
+            siekiera.transform.Rotate(0.0f, 0.0f, 90.0f);
+        }
+    }
+}
